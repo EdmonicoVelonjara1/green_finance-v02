307425afb96cce52cbb9398e2b9555e1d87f96b2
EdmonicoVelonjara1 <edmonico19@gmail.com>
2025-05-20 03:45:47 +0300

    Supprimer fichier.ts du suivi Git

--- lib/db.ts
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2/promise'
-
-export const db = mysql.createPool({
-    host: 'localhost',
-    user: 'venico',
-    password: 'venicoisme',
-    port: 3306,
-    database: 'green_finance'
-});
